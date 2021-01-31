b5d13d4e3d4f9e5515ec74a2f26e77112a9c4f15
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-01-31 19:42:47 -0300

    Added code in index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const regex = /<pre[^>]*>([\s\S]*?)<\/pre>/igm;
+
+hexo.extend.filter.register('after_post_render', data => {
+    if (regex.test(data.content)) {
+        data.content = data.content.replace(regex, (str, group1) => {
+            return str.replace(group1, '<button hidden aria-hidden="true">Copy</button>' + group1)
+        })
+    }
+    
+    data.content += `
+        <script>
+            document.querySelectorAll("button").forEach(function(button) {
+                button.hidden = false
+                button.addEventListener("click", function(e) {
+                    var range = document.createRange()
+                    range.selectNode(e.target.nextElementSibling)
+                    window.getSelection().removeAllRanges()
+                    window.getSelection().addRange(range)
+                    document.execCommand("copy")
+                    window.getSelection().removeAllRanges()
+
+                    button.innerText = 'Copied!'
+
+                    setTimeout(function() {
+                        button.innerText = 'Copy'
+                    }, 1500)
+                })          
+            })
+        </script>
+    `
+}, 9)
