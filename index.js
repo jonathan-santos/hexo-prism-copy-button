2535cebc30eb29af6a690e0998d35dfc0a24e542
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-02-20 10:12:50 -0300

    Adding tabindex='-1' to button

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const regex = /<pre[^>]*>([\s\S]*?)<\/pre>/igm;
 hexo.extend.filter.register('after_post_render', data => {
     if (regex.test(data.content)) {
         data.content = data.content.replace(regex, (str, group1) => {
-            return str.replace(group1, '<button hidden aria-hidden="true">Copy</button>' + group1)
+            return str.replace(group1, '<button hidden tabindex="-1" aria-hidden="true">Copy</button>' + group1)
         })
     }
     
